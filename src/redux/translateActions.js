6f7a6c1309748174d52453ddf42784a42941bc01
Halit Sancaktar <141269714+halitsancaktar34@users.noreply.github.com>
2025-01-02 16:31:54 +0300

    Update translateActions.js

--- src/redux/translateActions.js
+++ src/redux/translateActions.js
@@ -25,8 +25,6 @@ export const translateText = createAsyncThunk(
       url: "https://text-translator2.p.rapidapi.com/translate",
       headers: {
         "content-type": "application/x-www-form-urlencoded",
-        "X-RapidAPI-Key": "53bbfd6a45mshba00130ed68b464p12d86djsn5d5b28fac991",
-        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
       },
       data: encodedParams,
     };
