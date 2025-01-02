2f36a9b664b014af297af848eac9ead62ba7ce1c
Halit Sancaktar <141269714+halitsancaktar34@users.noreply.github.com>
2025-01-02 16:30:53 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,5 @@ export const options = {
   method: 'GET',
   url: 'https://text-translator2.p.rapidapi.com/getLanguages',
   headers: {
-    'X-RapidAPI-Key': '53bbfd6a45mshba00130ed68b464p12d86djsn5d5b28fac991',
-    'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
   }
 };
